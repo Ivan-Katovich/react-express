6f741cf8b2a3c96cde82f2454702c9b1d6066495
Ivan Katovich <kotovich.ivan@gmail.com>
2017-06-01 12:32:13 +0300

    add table

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+'/ind.html');
 });
 
+app.get('/table',function(req,res){
+    res.sendFile(__dirname+'/example.html');
+});
+
 app.listen(app.get('port'), function() {
     console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
